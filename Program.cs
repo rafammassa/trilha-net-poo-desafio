4313ab0d078b99e78a3c1b7d9bcc66e6d19fecda
rafammassa <rafaella_massa@hotmail.com>
2024-10-23 13:53:14 -0300

    Finalizando todos os ToDo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia("123", "Plus", "11111", 10);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("Smartphone Iphone: ");
+Smartphone iphone = new Iphone("321", "15", "2222", 15);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
