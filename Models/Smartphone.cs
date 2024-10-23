e5153bfe094d1e10581bdb1bd2c8eeca7cc74a63
rafammassa <rafaella_massa@hotmail.com>
2024-10-23 13:36:50 -0300

    Passando os parâmetros do construtor para as propriedades Smartphone

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -7,12 +7,15 @@ namespace DesafioPOO.Models
         protected string IMEI { get; set; }
         protected int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(){}
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
-
         public void Ligar()
         {
             Console.WriteLine("Ligando...");
