75276a2b612286c623871f3e47952762bf2a7b92
rafammassa <rafaella_massa@hotmail.com>
2024-10-23 13:12:00 -0300

    Implementando as propriedades faltantes em Smartphone

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,7 +3,9 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        protected string Modelo { get; set; }
+        protected string IMEI { get; set; }
+        protected int Memoria { get; set; }
 
         public Smartphone(string numero)
         {
